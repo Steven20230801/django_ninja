e803057ae0d8929b43ce1b0ce60edce2135fe012
steven <stevensu20230101@gmail.com>
2024-10-06 17:15:56 +0800

    bar chart

--- blog/blog/api.py
+++ blog/blog/api.py
@@ -1,5 +1,6 @@
 import json
 import os
+from django.conf import settings
 import pandas as pd
 from ninja import NinjaAPI
 from django.shortcuts import redirect, render
@@ -24,6 +25,12 @@ def hello(request):
 def home(request):
     context = {"message": "Hello, Django Ninja Templates!"}
     return render(request, "home.html", context)
+
+
+@api.get("/test")
+def home(request):
+    context = {"message": "Hello, Django Ninja Templates!"}
+    return render(request, "test.html", context)
 
 
 class AuthSchema(Schema):
@@ -88,13 +95,34 @@ def protected(request):
 # show
 @api.get("/show")
 def show(request):
-    pwd = os.getcwd()
-    # 設定工作目錄在/home/steven/python/django_ninja/blog
+    try:
+        # 獲取項目根目錄
+        project_root = settings.BASE_DIR  # 確保在 settings.py 中定義了 BASE_DIR
 
-    os.chdir("/home/steven/python/django_ninja/blog")
+        # 構建 ivr.xlsx 的絕對路徑
+        excel_path = os.path.join(project_root, "ivr.xlsx")
 
-    # 讀取ivr.xlsx
-    test = pd.read_excel("ivr.xlsx")
-    columns = test.columns
-    # return columns
-    return {"message": columns}
+        # 讀取 ivr.xlsx
+        test = pd.read_excel(excel_path)
+
+        # 準備 rebate_account 欄位的出現次數資料
+        rebate_account_counts = {}
+        if "rebate_account" in test.columns:
+            rebate_account_counts = test["rebate_account"].value_counts()
+            # 只篩選出現次數大於 2 的資料
+            rebate_account_counts = rebate_account_counts[rebate_account_counts > 2]
+            # 降序排列前20筆資料
+            rebate_account_counts = rebate_account_counts.head(20)
+
+            rebate_account_counts = rebate_account_counts.to_dict()
+
+        # 將 rebate_account_counts 序列化為 JSON
+        rebate_account_counts_json = rebate_account_counts
+        # rebate_account_counts_json = json.dumps(rebate_account_counts)
+    except Exception as e:
+        # 處理錯誤並傳遞空的資料或錯誤訊息
+        print(f"Error: {e}")
+        rebate_account_counts_json = json.dumps({})
+
+    # 導向 test.html，並傳遞 rebate_account_counts_json 資料
+    return render(request, "test.html", {"rebate_account_counts_json": rebate_account_counts_json, "username": request.user.username if request.user.is_authenticated else None})
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from .api import api
 
-urlpatterns = [path("admin/", admin.site.urls), path("", api.urls)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", api.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
+]
